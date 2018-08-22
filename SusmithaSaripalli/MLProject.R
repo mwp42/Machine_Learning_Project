b5385f207db5d9b8b7cacd4304f8a91849c6d219
ssaripalli <susmitha.saripalli@gmail.com>
2018-08-22 18:24:42 -0400

    addLevel update

--- SusmithaSaripalli/MLProject.R
+++ SusmithaSaripalli/MLProject.R
@@ -11,29 +11,14 @@ library(randomForest)
 dtest <- read.csv("/Users/susmithasaripalli/Documents/NYCDSA/machinelearningproj/all/test.csv", stringsAsFactors = TRUE)
 dtrain <- read.csv("/Users/susmithasaripalli/Documents/NYCDSA/machinelearningproj/all/train.csv", stringsAsFactors = TRUE)
 
-# # Quick look at data
-# summary(dtest)
-# describe(dtest)
-# summary(dtrain)
-# describe(dtrain)
-# # Plot missingness
-# image(is.na(dtest), main = "Missing Values", xlab = "Observation", ylab = "Variable", 
-#       xaxt = "n", yaxt = "n", bty = "n")
-# axis(1, seq(0, 1, length.out = nrow(dtest)), 1:nrow(dtest), col = "white")
-# axis(2, c(0, 0.5, 1), names(dtest), col = "white", las = 2)
-# 
-# image(is.na(dtrain), main = "Missing Values", xlab = "Observation", ylab = "Variable", 
-#       xaxt = "n", yaxt = "n", bty = "n")
-# axis(1, seq(0, 1, length.out = nrow(dtrain)), 1:nrow(dtrain), col = "white")
-# axis(2, c(0, 0.5, 1), names(dtrain), col = "white", las = 2)
-
 
 # Defining cleaning functions:
 
 # add "None" as a level for group1
 addLevel <- function(x){
-  if(is.factor(x)) return(factor(x, levels=c(levels(x), "None")))
-  return(x)
+  ifelse(is.factor(x), 
+         return(factor(x, levels=c(levels(x), "None"))), 
+         return(x))
 }
 
 # For the rest columns, qualitative assign the most frequest, quantitative assign 0
@@ -62,8 +47,9 @@ transFun <- function(t) {
   group2 <-t[,!(colnames(t) %in% cols)]
   
   # adding "None" level
-  group1 <- as.data.frame(lapply(group1, addLevel))
   group1[is.na(group1)] <- "None"
+  group1 <- as.data.frame(lapply(group1, addLevel))
+
   
   # Assigning 0 or most regularly occuring value to variable
   group2=data.table(group2)
@@ -82,9 +68,10 @@ nTrain = transFun(dtrain)
 nTest = transFun(dtest)
 
 # Quick fix for errors
-nTrain1 <- nTrain[complete.cases(nTrain), ]
+#nTrain1 <- nTrain[complete.cases(nTrain), ]
 
 # Implementing Random Forest
+set.seed(0)
 model1 <- randomForest(SalePrice ~ ., data = nTrain1, ntree = 1000, mtry =10, importance = TRUE)
 model1
 varImpPlot(model1)
